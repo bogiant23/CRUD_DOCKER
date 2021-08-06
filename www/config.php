66e657518ec0907db670197bc77b12e36fcd4c4f
bogiant23 <raskarowanabogiant@gmail.com>
2021-08-07 00:08:56 +0700

    upload file

---
+++ www/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$databaseHost = 'moe-mysql-app';
+$databaseName = 'moe_db';
+$databaseUsername = 'moeuser';
+$databasePassword = 'moepass';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
